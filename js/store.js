344e1dfa3ddd3c9d75196b2af589e86942f000cd
dh_lemanh <lemanh@demo.co.jp>
2021-04-15 00:22:33 +0900

    js

--- js/store.js
+++ js/store.js
@@ -4,15 +4,7 @@ if(document.readyState == 'loading'){
 	ready()
 }
 function ready(){
-	// remove button
-	var removeCartItemButton = document.getElementsByClassName('btn-danger')
-	console.log(removeCartItemButton)
 
-	// xoa button
-	/*for(var i = 0; i <removeCartItemButton.length; i++ ){
-		var button = removeCartItemButton[i]
-		button.addEventListener('click', removeCartItem)
-	}*/
 
 	// chinh sua input khong bi am
 	var quantityInputs = document.getElementsByClassName('cart-quantity-input')
@@ -20,35 +12,37 @@ function ready(){
 		var input = quantityInputs[i]
 		input.addEventListener('change', quantityChanged)
 	}
+	// show value
 
-	// add cart
-	var addToCartButton = document.getElementsByClassName('shop-item-button')
-	for(var i = 0; i<addToCartButton.length; i ++){
-		var button = addToCartButton[i]
-		button.addEventListener('click', addItemToCart)
-
+	var renderShowValue = document.getElementsByClassName('show-value')[0];
+	for(var i = 0; i < renderShowValue.length; i ++){
+		var input = renderShowValue[i]
+		console.log(input)
 	}
-	$('.shop-item-button').click(function(){
-		//UpDownPlus()
 
-		loadvalue()
-
-		//$('#rec_mode').find('option').not(':nth-child(n+ 5)').remove();
-
+	// loop dropdowns
+	$('.cart-items .cart-row').each(function(){
+		var thisRemove = $(this);
+		console.log(thisRemove)
+		var renderShowValue = $(this).find('.cart-price')
+		$(this).on('change', function() {
+		    var value = $('option:selected', this).val().replace(/Value\s/, '');
+		    $(renderShowValue).text(value)
+		    //console.log(value)
+		    updateCartTotal()
+		});
+		var btn = $(this).find('.btn-danger')
+		var removeCart = $(this).find('.cart-row')
+		//console.log(removeCart)
+		$(btn).on('click',function(e){
+			$(renderShowValue).text(0)
+			updateCartTotal()
+			thisRemove.remove()
+			return;
+		})
 	})
-
 	
-
-	// test
-
 	
-	loadvalue()
-	UpDownPlus()
-		
-	
-}
-function UpDownPlus(){
-	// tang giam 
 	$('.button').each(function(){
 		$(this).on("click", function() {
 
@@ -71,63 +65,9 @@ function UpDownPlus(){
 
 	  	});
 	})
+		
+	
 }
-
-function loadvalue(){
-	$('.cart-items .cart-row').each(function(index){
-		var mapRenderValue = $(this).find('.map-render').length
-		var showValue = $(this).find('.show-value').length
-
-		if(mapRenderValue > 0){
-			$(this).find('.map-render').addClass('map-render_' + index)
-		}
-
-		if(showValue > 0){
-			$(this).find('.show-value').addClass('show-value_' + index)
-		}
-
-		var options =
-		[
-			{
-			    "text"  : "選択してください。",
-			    "value" : "0",
-			    "selected" : true
-			},
-			{
-			    "text"  : "本",
-			    "value" : "1000"
-			},
-			{
-			    "text"     : "車",
-			    "value"    : "2000",
-			    
-			},
-			{
-			    "text"  : "自転車",
-			    "value" : "5000"
-			}
-		];
-
-		var selectBox = document.getElementsByClassName('map-render_' + index)[0];
-		var renderShowValue = document.getElementsByClassName('show-value_' + index)[0];
-		
-		// render select option
-		for(var i = 0, l = options.length; i < l; i++){
-		  var option = options[i];
-		  selectBox.options.add( new Option(option.text, option.value, option.selected) );
-		}
-		
-		$(this).on('change', function() {
-		    var value = $('option:selected', this).val().replace(/Value\s/, '');
-		    $(renderShowValue).text(value)
-		    console.log(value)
-		    updateCartTotal()
-		});
-
-		
-	})
-}
-// xoa button
 
 // them so luong hang khong bi am
 function quantityChanged(event){
@@ -139,45 +79,36 @@ function quantityChanged(event){
 }
 // add cart item
 
-
-function addItemToCart(){
-	var cartRow = document.createElement('div')
-	cartRow.classList.add('cart-row')
-	var cartItems = document.getElementsByClassName('cart-items')[0]
-	var cartItemsName = document.getElementsByClassName('cart-item-title')
-	var cartRowContents = `
-		<div class="row">
-            <div class="col-4">
-                <div class="cart-item cart-column">
-                    <select id="rec_mode" class="map-render"></select>
-                </div>
-            </div>
-            <div class="col-4">
-                <span class="cart-price cart-column show-value show-value_0" id="show_only">0</span>
-            </div>
-            <div class="col-4">
-                
-                <div class="cart-quantity cart-column">
-                    <div class="d-flex">
-                        <div class="dec button">-</div>
-                        <input class="cart-quantity-input" type="number" value="1">
-                        <div class="inc button">+</div>
-                        <button class="btn btn-danger" type="button">× 削除</button>
-                    </div>
-                </div>
-                
-            </div>
-        </div>
-	`
-	cartRow.innerHTML = cartRowContents
-
-	cartItems.append(cartRow)
-	// xoa nut remove
-	//cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
-	// thay doi so luong
-	cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+function pushcartItem(){
+	setTimeout(() => {
+		document.getElementById("pushCart\\").innerHTML += 
+              `<div class="cart-row">
+				<div class="row">
+					<div class="col-4">
+						<div class="cart-item cart-column">
+							<select id="rec_mode" class="map-render map-render_0"><option value="0" selected="">選択してください。</option><option value="1000">本</option><option value="2000">車</option><option value="5000">自転車</option></select>
+						</div>
+					</div>
+					<div class="col-4">
+						<span class="cart-price cart-column show-value show-value_0" id="show_only">0</span>
+					</div>
+					<div class="col-4">
+						
+						<div class="cart-quantity cart-column">
+							<div class="d-flex">
+								<div class="dec button">-</div>
+								<input class="cart-quantity-input" type="number" value="1">
+								<div class="inc button">+</div>
+								<button class="btn btn-danger" type="button" value="1">× 削除</button>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+`;
+	}, 3000);
 }
-
 // thay doi gia tien khi xoa button
 function updateCartTotal(){
 	var cartItemContainer = document.getElementsByClassName('cart-items')[0]
